b49e8f4736fe41aee2528b7ec8dae7d5dd9cb031
Joseph Koh <joseph0750@gmail.com>
2024-05-14 18:19:57 +0800

    add new feature

--- YSTools-Swift/Classes/Extension/UINavigationController+Pop.swift
+++ YSTools-Swift/Classes/Extension/UINavigationController+Pop.swift
@@ -25,34 +25,31 @@ public extension UINavigationController {
         }
         self.setViewControllers(updatedViewControllers, animated: false)
     }
-    
-    public func pushViewController(
-        _ viewController: UIViewController,
-        animated: Bool,
-        completion: @escaping () -> Void)
-    {
+
+    func pushViewController(_ viewController: UIViewController, animated: Bool, completion: @escaping () -> Void) {
         pushViewController(viewController, animated: animated)
-
         guard animated, let coordinator = transitionCoordinator else {
             DispatchQueue.main.async { completion() }
             return
         }
-
         coordinator.animate(alongsideTransition: nil) { _ in completion() }
     }
 
-    func popViewController(
-        animated: Bool,
-        completion: @escaping () -> Void)
-    {
+    func popViewController(animated: Bool, completion: @escaping () -> Void) {
         popViewController(animated: animated)
-
         guard animated, let coordinator = transitionCoordinator else {
             DispatchQueue.main.async { completion() }
             return
         }
-
         coordinator.animate(alongsideTransition: nil) { _ in completion() }
     }
 
+    func popToRootViewController(animated: Bool, completion: @escaping () -> Void) {
+        popToRootViewController(animated: animated)
+        guard animated, let coordinator = transitionCoordinator else {
+            DispatchQueue.main.async { completion() }
+            return
+        }
+        coordinator.animate(alongsideTransition: nil) { _ in completion() }
+    }
 }
