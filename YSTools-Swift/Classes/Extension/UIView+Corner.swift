bf121a2a8cd540dc5712d4463cb8e8103468c688
Joseph <peanutgao@hotmail.com>
2025-07-18 14:42:56 +0800

    优化圆角实现

--- YSTools-Swift/Classes/Extension/UIView+Corner.swift
+++ YSTools-Swift/Classes/Extension/UIView+Corner.swift
@@ -9,16 +9,73 @@
 import UIKit
 
 public extension UIView {
+    private static var roundCornersLayerKey: UInt8 = 0
+    private static var roundCornersConfigKey: UInt8 = 0
+
+    private struct RoundCornersConfig {
+        let corners: UIRectCorner
+        let radius: CGFloat
+    }
+
+    private var roundCornersMaskLayer: CAShapeLayer? {
+        get {
+            objc_getAssociatedObject(self, &UIView.roundCornersLayerKey) as? CAShapeLayer
+        }
+        set {
+            objc_setAssociatedObject(self, &UIView.roundCornersLayerKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    private var roundCornersConfig: RoundCornersConfig? {
+        get {
+            objc_getAssociatedObject(self, &UIView.roundCornersConfigKey) as? RoundCornersConfig
+        }
+        set {
+            objc_setAssociatedObject(self, &UIView.roundCornersConfigKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
     @discardableResult
     func addRoundCorners(corners: UIRectCorner, radius: CGFloat) -> Self {
-        let path = UIBezierPath(
-            roundedRect: bounds,
-            byRoundingCorners: corners,
-            cornerRadii: CGSize(width: radius, height: radius)
-        )
-        let maskLayer = CAShapeLayer()
-        maskLayer.path = path.cgPath
-        layer.mask = maskLayer
+        DispatchQueue.main.async { [weak self] in
+            guard let self, !bounds.isEmpty else {
+                return
+            }
+            createOrUpdateRoundCornersMask(corners: corners, radius: radius)
+        }
+        return self
+    }
+
+    @discardableResult
+    func updateRoundCorners(corners: UIRectCorner, radius: CGFloat) -> Self {
+        guard !bounds.isEmpty else {
+            return self
+        }
+
+        createOrUpdateRoundCornersMask(corners: corners, radius: radius)
+        return self
+    }
+
+    @discardableResult
+    func removeRoundCorners() -> Self {
+        if let roundCornersMask = roundCornersMaskLayer {
+            roundCornersMask.removeFromSuperlayer()
+            roundCornersMaskLayer = nil
+            roundCornersConfig = nil
+
+            if layer.mask == roundCornersMask {
+                layer.mask = nil
+            }
+        }
+        return self
+    }
+
+    @discardableResult
+    func updateRoundCornersIfNeeded() -> Self {
+        guard let config = roundCornersConfig else {
+            return self
+        }
+        updateRoundCorners(corners: config.corners, radius: config.radius)
         return self
     }
 
@@ -30,5 +87,38 @@ public extension UIView {
         layer.shadowRadius = radius
         layer.shadowOpacity = opacity
         return self
+    }
+
+    private func createOrUpdateRoundCornersMask(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(
+            roundedRect: bounds,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+
+        if let existingMask = roundCornersMaskLayer {
+            existingMask.path = path.cgPath
+            existingMask.frame = bounds
+        }
+        else {
+            let maskLayer = CAShapeLayer()
+            maskLayer.path = path.cgPath
+            maskLayer.frame = bounds
+
+            if let existingMask = layer.mask {
+                let combinedMask = CALayer()
+                combinedMask.frame = bounds
+                combinedMask.addSublayer(existingMask)
+                combinedMask.addSublayer(maskLayer)
+                layer.mask = combinedMask
+            }
+            else {
+                layer.mask = maskLayer
+            }
+
+            roundCornersMaskLayer = maskLayer
+        }
+
+        roundCornersConfig = RoundCornersConfig(corners: corners, radius: radius)
     }
 }
