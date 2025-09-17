f880ba5a42e82db6510f8702a1f31f7a2fcf1adb
Joseph <peanutgao@hotmail.com>
2025-09-17 13:38:58 +0800

    bugfix

--- YSTools-Swift/Classes/Extension/UIButton+ImagePosition.swift
+++ YSTools-Swift/Classes/Extension/UIButton+ImagePosition.swift
@@ -6,118 +6,167 @@
 //  Copyright © 2019 Joseph Koh. All rights reserved.
 //
 
+import ObjectiveC
 import UIKit
 
 // MARK: - UIButton.ImagePositionType
 
-public extension UIButton {
-    enum ImagePositionType: Int {
-        case left
-        case right
-        case top
-        case bottom
-    }
+extension UIButton {
+  public enum ImagePositionType: Int {
+    case left
+    case right
+    case top
+    case bottom
+  }
 }
 
-public extension UIButton {
-    @discardableResult
-    func ys_setImagePosition(_ position: ImagePositionType, withMargin margin: CGFloat) -> Self {
-        setTitle(currentTitle, for: .normal)
-        setImage(currentImage, for: .normal)
-
-        let imageWidth = imageView?.image?.size.width ?? 0
-        let imageHeight = imageView?.image?.size.height ?? 0
-        let labelWidth = self.textSize().width
-        let labelHeight = self.textSize().height
-
-        let imageOffsetX = (imageWidth + labelWidth) * 0.5 - imageWidth * 0.5
-        let imageOffsetY = imageHeight * 0.5 + margin * 0.5
-        let labelOffsetX = (imageWidth + labelWidth * 0.5) - (imageWidth + labelWidth) * 0.5
-        let labelOffsetY = labelHeight * 0.5 + margin * 0.5
-
-        let tempWidth = max(labelWidth, imageWidth)
-        let changedWidth = labelWidth + imageWidth - tempWidth
-        let tempHeight = max(labelHeight, imageHeight)
-        let changedHeight = labelHeight + imageHeight + margin - tempHeight
-
-        switch position {
-        case .left:
-            self.imageEdgeInsets = UIEdgeInsets(top: 0, left: -margin * 0.5, bottom: 0, right: margin * 0.5)
-            self.titleEdgeInsets = UIEdgeInsets(top: 0, left: margin * 0.5, bottom: 0, right: -margin * 0.5)
-            self.contentEdgeInsets = UIEdgeInsets(top: 0, left: margin * 0.5, bottom: 0, right: margin * 0.5)
-
-        case .right:
-            self.imageEdgeInsets = UIEdgeInsets(
-                top: 0,
-                left: labelWidth + margin * 0.5,
-                bottom: 0,
-                right: -(labelWidth + margin * 0.5)
-            )
-            self.titleEdgeInsets = UIEdgeInsets(
-                top: 0,
-                left: -(imageWidth + margin * 0.5),
-                bottom: 0,
-                right: imageWidth + margin * 0.5
-            )
-            self.contentEdgeInsets = UIEdgeInsets(top: 0, left: margin * 0.5, bottom: 0, right: margin * 0.5)
-
-        case .top:
-            self.imageEdgeInsets = UIEdgeInsets(
-                top: -imageOffsetY,
-                left: imageOffsetX,
-                bottom: imageOffsetY,
-                right: -imageOffsetX
-            )
-            self.titleEdgeInsets = UIEdgeInsets(
-                top: labelOffsetY,
-                left: -labelOffsetX,
-                bottom: -labelOffsetY,
-                right: labelOffsetX
-            )
-            self.contentEdgeInsets = UIEdgeInsets(
-                top: imageOffsetY,
-                left: -changedWidth * 0.5,
-                bottom: changedHeight - imageOffsetY,
-                right: -changedWidth * 0.5
-            )
-
-        case .bottom:
-            self.imageEdgeInsets = UIEdgeInsets(
-                top: imageOffsetY,
-                left: imageOffsetX,
-                bottom: -imageOffsetY,
-                right: -imageOffsetX
-            )
-            self.titleEdgeInsets = UIEdgeInsets(
-                top: -labelOffsetY,
-                left: -labelOffsetX,
-                bottom: labelOffsetY,
-                right: labelOffsetX
-            )
-            self.contentEdgeInsets = UIEdgeInsets(
-                top: changedHeight - imageOffsetY,
-                left: -changedWidth * 0.5,
-                bottom: imageOffsetY,
-                right: -changedWidth * 0.5
-            )
-        }
-        return self
-    }
+extension UIButton {
+  @discardableResult
+  public func ys_setImagePosition(_ position: ImagePositionType, withMargin margin: CGFloat) -> Self
+  {
+    UIButton.ys_swizzleLayoutIfNeeded()
+    ys_positionRaw = position.rawValue
+    ys_margin = margin
+    setNeedsLayout()
+    ys_applyImagePositionIfNeeded()
+    return self
+  }
 }
 
-private extension UIButton {
-    func textSize() -> CGSize {
-        let str = titleLabel?.text as NSString?
-        let size = str?.boundingRect(
-            with: CGSize(width: CGFloat.greatestFiniteMagnitude, height: CGFloat.greatestFiniteMagnitude),
-            options: NSStringDrawingOptions.usesLineFragmentOrigin,
-            attributes: [.font: titleLabel?.font as Any],
-            context: nil
-        ).size
-        guard let strSize = size else {
-            return CGSize.zero
-        }
-
-        return CGSize(width: ceil(strSize.width), height: ceil(strSize.height))
+extension UIButton {
+  fileprivate func ys_currentTextSize() -> CGSize {
+    if let label = titleLabel {
+      let size = label.intrinsicContentSize
+      if size.width.isFinite, size.height.isFinite {
+        return CGSize(width: ceil(size.width), height: ceil(size.height))
+      }
     }
+    return .zero
+  }
+}
+
+// MARK: - 关联属性 & 动态布局
+
+private var ysPositionKey: UInt8 = 0
+private var ysMarginKey: UInt8 = 0
+private var ysDidSwizzleKey: UInt8 = 0
+
+extension UIButton {
+  fileprivate var ys_positionRaw: Int? {
+    get { objc_getAssociatedObject(self, &ysPositionKey) as? Int }
+    set {
+      objc_setAssociatedObject(self, &ysPositionKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+  fileprivate var ys_margin: CGFloat {
+    get { (objc_getAssociatedObject(self, &ysMarginKey) as? CGFloat) ?? 0 }
+    set {
+      objc_setAssociatedObject(self, &ysMarginKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+
+  fileprivate static func ys_swizzleLayoutIfNeeded() {
+    let did = objc_getAssociatedObject(self, &ysDidSwizzleKey) as? Bool ?? false
+    guard !did else { return }
+    guard
+      let original = class_getInstanceMethod(UIButton.self, #selector(UIButton.layoutSubviews)),
+      let swizzled = class_getInstanceMethod(UIButton.self, #selector(UIButton.ys_layoutSubviews))
+    else { return }
+    method_exchangeImplementations(original, swizzled)
+    objc_setAssociatedObject(self, &ysDidSwizzleKey, true, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+  }
+
+  @objc fileprivate func ys_layoutSubviews() {
+    ys_layoutSubviews()
+    ys_applyImagePositionIfNeeded()
+  }
+
+  fileprivate func ys_applyImagePositionIfNeeded() {
+    guard let raw = ys_positionRaw, let position = ImagePositionType(rawValue: raw) else { return }
+    let margin = ys_margin
+
+    let imageWidth = imageView?.image?.size.width ?? 0
+    let imageHeight = imageView?.image?.size.height ?? 0
+    let textSize = ys_currentTextSize()
+    let labelWidth = textSize.width
+    let labelHeight = textSize.height
+
+    if imageWidth == 0, imageHeight == 0, labelWidth == 0, labelHeight == 0 { return }
+
+    let imageOffsetX = (imageWidth + labelWidth) * 0.5 - imageWidth * 0.5
+    let imageOffsetY = imageHeight * 0.5 + margin * 0.5
+    let labelOffsetX = (imageWidth + labelWidth * 0.5) - (imageWidth + labelWidth) * 0.5
+    let labelOffsetY = labelHeight * 0.5 + margin * 0.5
+
+    let tempWidth = max(labelWidth, imageWidth)
+    let changedWidth = labelWidth + imageWidth - tempWidth
+    let tempHeight = max(labelHeight, imageHeight)
+    let changedHeight = labelHeight + imageHeight + margin - tempHeight
+
+    switch position {
+    case .left:
+      self.imageEdgeInsets = UIEdgeInsets(
+        top: 0, left: -margin * 0.5, bottom: 0, right: margin * 0.5)
+      self.titleEdgeInsets = UIEdgeInsets(
+        top: 0, left: margin * 0.5, bottom: 0, right: -margin * 0.5)
+      self.contentEdgeInsets = UIEdgeInsets(
+        top: 0, left: margin * 0.5, bottom: 0, right: margin * 0.5)
+
+    case .right:
+      self.imageEdgeInsets = UIEdgeInsets(
+        top: 0,
+        left: labelWidth + margin * 0.5,
+        bottom: 0,
+        right: -(labelWidth + margin * 0.5)
+      )
+      self.titleEdgeInsets = UIEdgeInsets(
+        top: 0,
+        left: -(imageWidth + margin * 0.5),
+        bottom: 0,
+        right: imageWidth + margin * 0.5
+      )
+      self.contentEdgeInsets = UIEdgeInsets(
+        top: 0, left: margin * 0.5, bottom: 0, right: margin * 0.5)
+
+    case .top:
+      self.imageEdgeInsets = UIEdgeInsets(
+        top: -imageOffsetY,
+        left: imageOffsetX,
+        bottom: imageOffsetY,
+        right: -imageOffsetX
+      )
+      self.titleEdgeInsets = UIEdgeInsets(
+        top: labelOffsetY,
+        left: -labelOffsetX,
+        bottom: -labelOffsetY,
+        right: labelOffsetX
+      )
+      self.contentEdgeInsets = UIEdgeInsets(
+        top: imageOffsetY,
+        left: -changedWidth * 0.5,
+        bottom: changedHeight - imageOffsetY,
+        right: -changedWidth * 0.5
+      )
+
+    case .bottom:
+      self.imageEdgeInsets = UIEdgeInsets(
+        top: imageOffsetY,
+        left: imageOffsetX,
+        bottom: -imageOffsetY,
+        right: -imageOffsetX
+      )
+      self.titleEdgeInsets = UIEdgeInsets(
+        top: -labelOffsetY,
+        left: -labelOffsetX,
+        bottom: labelOffsetY,
+        right: labelOffsetX
+      )
+      self.contentEdgeInsets = UIEdgeInsets(
+        top: changedHeight - imageOffsetY,
+        left: -changedWidth * 0.5,
+        bottom: imageOffsetY,
+        right: -changedWidth * 0.5
+      )
+    }
+  }
 }
--- YSTools-Swift/Classes/Extension/UIButton+Throttle.swift
+++ YSTools-Swift/Classes/Extension/UIButton+Throttle.swift
@@ -12,65 +12,128 @@ import UIKit
 private var throttleIntervalKey: UInt8 = 0
 private var isThrottlingKey: UInt8 = 0
 private var throttledActionKey: UInt8 = 0
+private var throttleGuardsInstalledKey: UInt8 = 0
+private var suppressWhileThrottlingKey: UInt8 = 0
+private var throttleEventsKey: UInt8 = 0
 
 extension UIButton {
-    private var throttleInterval: TimeInterval {
-        get {
-            objc_getAssociatedObject(self, &throttleIntervalKey) as? TimeInterval ?? 0
-        }
-        set {
-            objc_setAssociatedObject(self, &throttleIntervalKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
+  private var throttleInterval: TimeInterval {
+    get {
+      objc_getAssociatedObject(self, &throttleIntervalKey) as? TimeInterval ?? 0
     }
-
-    private var isThrottling: Bool {
-        get {
-            objc_getAssociatedObject(self, &isThrottlingKey) as? Bool ?? false
-        }
-        set {
-            objc_setAssociatedObject(self, &isThrottlingKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
+    set {
+      objc_setAssociatedObject(
+        self, &throttleIntervalKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
+  }
 
-    private var throttledAction: ((UIButton) -> Void)? {
-        get {
-            objc_getAssociatedObject(self, &throttledActionKey) as? (UIButton) -> Void
-        }
-        set {
-            objc_setAssociatedObject(self, &throttledActionKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        }
+  private var isThrottling: Bool {
+    get {
+      objc_getAssociatedObject(self, &isThrottlingKey) as? Bool ?? false
     }
-
-    @objc private func throttledClick(_ sender: UIButton) {
-        if isThrottling {
-            debugPrint("click action had been throttled!!!")
-            return
-        }
-        isThrottling = true
-
-        sender.throttledAction?(sender)
-        DispatchQueue.main.asyncAfter(deadline: .now() + self.throttleInterval) { [weak self] in
-            self?.isThrottling = false
-        }
+    set {
+      objc_setAssociatedObject(self, &isThrottlingKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
+  }
 
-    public func addThrottledAction(
-            interval: TimeInterval = 1.0,
-            for event: UIControl.Event,
-            action: @escaping (UIButton) -> Void
-    ) {
-        _ = ys_addThrottledAction(interval: interval, for: event, action: action)
+  private var throttledAction: ((UIButton) -> Void)? {
+    get {
+      objc_getAssociatedObject(self, &throttledActionKey) as? (UIButton) -> Void
     }
+    set {
+      objc_setAssociatedObject(
+        self, &throttledActionKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
 
-    @discardableResult
-    public func ys_addThrottledAction(
-            interval: TimeInterval = 1.0,
-            for event: UIControl.Event,
-            action: @escaping (UIButton) -> Void
-    ) -> Self {
-        self.throttleInterval = interval
-        self.throttledAction = action
-        addTarget(self, action: #selector(throttledClick(_:)), for: event)
-        return self
+  private var throttleGuardsInstalled: Bool {
+    get {
+      objc_getAssociatedObject(self, &throttleGuardsInstalledKey) as? Bool ?? false
     }
+    set {
+      objc_setAssociatedObject(
+        self, &throttleGuardsInstalledKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+
+  // 以 raw 值的方式记录已注册的事件掩码，避免直接桥接 OptionSet 导致的不确定性
+  private var throttleEventsRaw: UInt {
+    get {
+      (objc_getAssociatedObject(self, &throttleEventsKey) as? UInt) ?? 0
+    }
+    set {
+      objc_setAssociatedObject(
+        self, &throttleEventsKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    }
+  }
+
+  private var throttleEvents: UIControl.Event {
+    get { UIControl.Event(rawValue: throttleEventsRaw) }
+    set { throttleEventsRaw = newValue.rawValue }
+  }
+
+  public var ysSuppressWhileThrottling: Bool {
+    get {
+      objc_getAssociatedObject(self, &suppressWhileThrottlingKey) as? Bool ?? true
+    }
+    set {
+      objc_setAssociatedObject(
+        self,
+        &suppressWhileThrottlingKey,
+        newValue,
+        .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+      )
+    }
+  }
+
+  @objc private func throttleTouchDown(_ sender: UIButton) {
+    guard ysSuppressWhileThrottling else { return }
+    guard isThrottling else { return }
+    sender.isHighlighted = false
+    sender.cancelTracking(with: nil)
+  }
+
+  @objc private func throttledClick(_ sender: UIButton) {
+    if isThrottling {
+      #if DEBUG
+        debugPrint("click action had been throttled!!!")
+      #endif
+      if ysSuppressWhileThrottling { sender.isHighlighted = false }
+      return
+    }
+    isThrottling = true
+
+    sender.throttledAction?(sender)
+    DispatchQueue.main.asyncAfter(deadline: .now() + self.throttleInterval) { [weak self] in
+      self?.isThrottling = false
+    }
+  }
+
+  public func addThrottledAction(
+    interval: TimeInterval = 1.0,
+    for event: UIControl.Event,
+    action: @escaping (UIButton) -> Void
+  ) {
+    _ = ys_addThrottledAction(interval: interval, for: event, action: action)
+  }
+
+  @discardableResult
+  public func ys_addThrottledAction(
+    interval: TimeInterval = 1.0,
+    for event: UIControl.Event,
+    action: @escaping (UIButton) -> Void
+  ) -> Self {
+    self.throttleInterval = interval
+    self.throttledAction = action
+
+    removeTarget(self, action: #selector(throttledClick(_:)), for: event)
+    addTarget(self, action: #selector(throttledClick(_:)), for: event)
+
+    self.throttleEvents = self.throttleEvents.union(event)
+    if !throttleGuardsInstalled {
+      addTarget(self, action: #selector(throttleTouchDown(_:)), for: .touchDown)
+      throttleGuardsInstalled = true
+    }
+    return self
+  }
 }
