d63df6746dd42670f71a9b5ce12178efdda812ca
Joseph <peanutgao@hotmail.com>
2025-11-03 14:16:57 +0800

    feat: UITableViewCell 分割线增加协议实现自定义配置

--- YSTools-Swift/Classes/Extension/UITableViewCell+Seperator.swift
+++ YSTools-Swift/Classes/Extension/UITableViewCell+Seperator.swift
@@ -7,6 +7,25 @@
 
 import UIKit
 
+// MARK: - AutoConfigurableSeparator
+
+/// Protocol to indicate if a cell should auto-configure separator
+public protocol AutoConfigurableSeparator {
+    /// Separator configuration for the cell
+    var separatorConfiguration: SeparatorConfiguration { get }
+    
+    /// Whether to enable auto-configuration of separator
+    var shouldAutoConfigureSeparator: Bool { get }
+}
+
+public extension AutoConfigurableSeparator {
+    /// Default implementation: enable auto-configuration
+    var shouldAutoConfigureSeparator: Bool { true }
+    
+    /// Default separator configuration
+    var separatorConfiguration: SeparatorConfiguration { .default }
+}
+
 // MARK: - SeparatorConfiguration
 
 /// Configuration for separator line styling
@@ -95,6 +114,41 @@ public extension UITableViewCell {
 
         configureSeparator(with: config, position: position, totalRows: totalRows)
     }
+    
+    /// Automatically configures separator based on protocol conformance
+    /// - Parameters:
+    ///   - tableView: The table view containing this cell
+    ///   - indexPath: Index path of the cell
+    /// - Note:
+    ///   - Conforms to protocol + enabled: use custom configuration
+    ///   - Conforms to protocol + disabled: remove separator
+    ///   - Does NOT conform to protocol: use default configuration
+    func autoConfigureSeparatorIfNeeded(
+        in tableView: UITableView,
+        at indexPath: IndexPath
+    ) {
+        if let configurableCell = self as? AutoConfigurableSeparator {
+            // Cell conforms to protocol
+            if configurableCell.shouldAutoConfigureSeparator {
+                // Explicitly enabled, use custom configuration
+                autoConfigureSeparator(
+                    in: tableView,
+                    at: indexPath,
+                    with: configurableCell.separatorConfiguration
+                )
+            } else {
+                // Explicitly disabled, remove separator
+                removeSeparator()
+            }
+        } else {
+            // Not conforming to protocol, use default configuration
+            autoConfigureSeparator(
+                in: tableView,
+                at: indexPath,
+                with: .default
+            )
+        }
+    }
 
     // MARK: - Private Methods
 
@@ -115,10 +169,8 @@ public extension UITableViewCell {
     }
 
     private func removeSeparator() {
-        for view in contentView.subviews {
-            if view.tag == SeparatorTag.value {
-                view.removeFromSuperview()
-            }
+        for view in contentView.subviews where view.tag == SeparatorTag.value {
+            view.removeFromSuperview()
         }
     }
 
